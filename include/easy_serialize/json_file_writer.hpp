3c81881bee83a59c8cbf85ff137153c619e01838
Matt Drees <matt@drees.me>
2022-11-08 15:05:26 -0800

    Moved file writing stuff to its own file.

---
+++ include/easy_serialize/json_file_writer.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+#include "easy_serialize_status.hpp"
+#include "json_writer.hpp"
+
+#include <cstdio>
+
+namespace easy_serialize
+{
+
+    // Helper function for writing json to a file.
+    EasySerializeStatus to_file(const std::string &filename, const std::string &json)
+    {
+        EasySerializeStatus status;
+        std::FILE *fp = fopen(filename.c_str(), "w");
+        if (!fp)
+        {
+            status.error_message = "File opening failed.";
+        }
+        else
+        {
+            std::fwrite(json.c_str(), json.size(), 1, fp);
+        }
+        fclose(fp);
+        return status;
+    }
+
+    // Write object UTF-8 JSON to file.
+    //
+    // \param filename: "path/to/filename.json"
+    // \param obj: object to write
+    // \param json_indent: JSON indent formatting
+    // \return status
+    template <typename T>
+    EasySerializeStatus to_json_file(const std::string &filename, T &obj,
+                                     JsonIndent json_indent = JsonIndent::two_spaces)
+    {
+        std::string json = to_json_string(obj, json_indent);
+        return to_file(filename, json);
+    }
+
+    // Write vector of objects UTF-8 JSON to a file.
+    //
+    // \param filename: "path/to/filename.json"
+    // \param v: vector of objects to write
+    // \param json_indent: JSON indent formatting
+    // \return status
+    template <typename T>
+    EasySerializeStatus to_json_file_vector_objects(const std::string &filename, std::vector<T> &v,
+                                                    JsonIndent json_indent = JsonIndent::two_spaces)
+    {
+        std::string json = to_json_string_vector_objects(v, json_indent);
+        return to_file(filename, json);
+    }
+
+    // Write vector of enums UTF-8 JSON to a file.
+    //
+    // \param filename: "path/to/filename.json"
+    // \param v: vector of enums to write
+    // \param json_indent: JSON indent formatting
+    // \return status
+    template <typename T>
+    EasySerializeStatus to_json_file_vector_enums(const std::string &filename, std::vector<T> &v,
+                                                  JsonIndent json_indent = JsonIndent::two_spaces)
+    {
+        std::string json = to_json_string_vector_enums(v, json_indent);
+        return to_file(filename, json);
+    }
+
+    // Write vector of supported C++ fundamental types UTF-8 JSON to a file.
+    //
+    // \param filename: "path/to/filename.json"
+    // \param v: vector of enums to write
+    // \param json_indent: JSON indent formatting
+    // \return status
+    template <typename T>
+    EasySerializeStatus to_json_file_vector(const std::string &filename, std::vector<T> &v,
+                                            JsonIndent json_indent = JsonIndent::two_spaces)
+    {
+        std::string json = to_json_string_vector(v, json_indent);
+        return to_file(filename, json);
+    }
+
+} // namespace easy_serialize
--- include/easy_serialize/json_writer.hpp
+++ include/easy_serialize/json_writer.hpp
@@ -8,37 +8,6 @@
 
 namespace easy_serialize
 {
-    // Write UTF-8 JSON to file.
-    //
-    // \param filename: "path/to/filename.json"
-    // \param obj: object to write
-    // \param json_indent: JSON indent formatting
-    // \return status
-    // template <typename T>
-    // EasySerializeStatus to_json_file(const std::string &filename, T &obj,
-    //                                  JsonIndent json_indent = JsonIndent::two_spaces)
-    // {
-    //     rapidjson::StringBuffer string_buffer;
-    //     rapidjson_impl::to_json_buffer(string_buffer, obj, json_indent);
-    //     EasySerializeStatus status;
-    //     File *fp = fopen(filename.c_str());
-    //     // TODO: file handling stuff
-    //     fclose(fp);
-    //     return status;
-    // }
-
-    // // Write vector of objects UTF-8 JSON to file.
-    // //
-    // // \param filename: "path/to/filename.json"
-    // // \param v: vector of objects to write
-    // // \param json_indent: JSON indent formatting
-    // // \return status
-    // template <typename T>
-    // EasySerializeStatus to_json_file(const std::string &filename, std::vector<T> &v,
-    //                                  JsonIndent json_indent = JsonIndent::two_spaces)
-    // {
-    // }
-
     // Write object UTF-8 JSON to a std::string.
     //
     // \param obj: object to write
